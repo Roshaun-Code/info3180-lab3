33296382766c81b6b341ea103135c21c0afdd882
Roshaun Marshall <roshaunmarshall@gmail.com>
2025-03-01 22:18:20 -0500

    Basically done

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app.forms import ContactForm
+from app import mail
+from flask_mail import Message
+
 
 
 ###
@@ -16,6 +20,26 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message("New Contact Form Submission",
+                      sender=("Sender Name", "your_email@example.com"),
+                      recipients=["recipient@example.com"])
+        msg.body = f"Message from {form.name.data}:\n\n{form.message.data}"
+        
+        try:
+            mail.send(msg)
+            flash(f'Message from {form.name.data} sent successfully!', 'success')
+            return redirect(url_for('contact'))
+        except Exception as e:
+            flash(f'Error sending message: {str(e)}', 'danger')
+            return redirect(url_for('contact'))
+    
+    flash_errors(form)  # Flash errors if validation fails
+    return render_template('contact.html', form=form)
 
 
 ###
